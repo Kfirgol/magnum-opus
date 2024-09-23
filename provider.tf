3d57fe08aaf2a046e686aafa9bf9dbb7fb85621c
Kfirgol <kfirgol100@gmail.com>
2024-09-23 19:18:45 +0300

    testing

--- provider.tf
+++ provider.tf
@@ -1,5 +1,11 @@
-
 terraform {
+  backend "s3" {
+    bucket         = "terraform-state-commit-eks"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true                    # Encrypt the state file at rest
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
